2b03e3ef2237d7e8c5bcbf01afc74d34a37f66cc
ibrahim2102 <mdibrahim91695@gmail.com>
2026-01-02 02:59:17 +0600

    done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 
 
@@ -36,8 +36,6 @@ app.get("/productss", (req, res) => {
   ];
   res.json(products);
 });
-
-
 
 
 
